755b2a01a01dd6aab862e2e751d76bcc31d5f884
kpisti18 <64091644+kpisti18@users.noreply.github.com>
2023-06-07 22:06:38 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,22 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Net;
+using System.Threading.Channels;
 
-namespace Kovacs_Istvan_01_GUI
+namespace Kovacs_Istvan_01_console
 {
-    internal static class Program
+    internal class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        static List<Ugyfel> ugyfelek = new List<Ugyfel>();
+        static List<Befizetes> befizetesek = new List<Befizetes>();
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormNyito());
+            loadingUgyfel();
+            loadingBefizetes();
+            elso();
+            masodik();
+            harmadik();
+            Console.WriteLine("Program vége!");
+            Console.ReadKey();
+        }
+
+        private static void harmadik()
+        {
+            string legfiatalabb = ugyfelek.OrderBy(x => x.Szulev).FirstOrDefault().Nev;
+            Console.WriteLine($"3. feladat: A legfiatalabb ügyfél: {legfiatalabb}");
+        }
+
+        private static void masodik()
+        {
+            ugyfelek.Where(x => x.Orsz.Equals("H")).ToList().ForEach(x => Console.WriteLine($"{x.Nev}: {x.Orsz}"));
+        }
+
+        private static void elso()
+        {
+            Console.WriteLine($"1. feladat: Ügyfelek száma: {ugyfelek.Count}");
+        }
+
+        private static void loadingBefizetes()
+        {
+            Database db = new Database();
+            befizetesek.AddRange(db.getBefizetes());
+        }
+
+        private static void loadingUgyfel()
+        {
+            Database db = new Database();
+            ugyfelek.AddRange(db.getUgyfel());           
         }
     }
 }
